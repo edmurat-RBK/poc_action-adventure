64c60510c10f42bf021bbd838cbf0db8e4a8d4a1
Edouard Murat <e.murat@rubika-edu.com>
2020-01-31 01:30:14 +0100

    Added player walk and idle animation

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float baseSpeed = 1f;
+    public float speedModifier = 1f;
+
+    private float inputHorizontal;
+    private float inputVertical;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private PlayerState playerState;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        playerState = GetComponent<PlayerState>();
+    }
+
+    private void Update()
+    {
+        if (playerState.CanWalk)
+        {
+            inputHorizontal = Input.GetAxis("Horizontal");
+            inputVertical = Input.GetAxis("Vertical");
+
+            Vector3 force = new Vector3(inputHorizontal, -inputVertical, 0f).normalized;
+            playerState.CurrentDirection = force;
+            rb.velocity = (force * (baseSpeed * speedModifier));
+
+            if(!force.Equals(Vector3.zero))
+            {
+                playerState.IsWalking = true;
+            }
+            else
+            {
+                playerState.IsWalking = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerState.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    private Vector3 currentDirection;
+    public Vector3 CurrentDirection
+    {
+        set
+        {
+            currentDirection = value;
+        }
+    }
+
+    private Vector3 lastDirection;
+    public Vector3 LastDirection
+    {
+        set
+        {
+            lastDirection = value;
+        }
+    }
+
+    private bool canWalk = true;
+    public bool CanWalk
+    {
+        get
+        {
+            return canWalk;
+        }
+
+        set
+        {
+            canWalk = value;
+        }
+    }
+
+    private bool isWalking = false;
+    public bool IsWalking
+    {
+        get
+        {
+            return isWalking;
+        }
+
+        set
+        {
+            isWalking = value;
+        }
+    }
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        lastDirection = new Vector3();
+    }
+
+    private void Update()
+    {
+        UpdateLastDirection();
+        UpdateAnimator();
+    }
+
+    private void UpdateLastDirection()
+    {
+        if(isWalking)
+        {
+            lastDirection.Set(currentDirection.x, currentDirection.y, currentDirection.z);
+        }
+    }
+
+    private void UpdateAnimator()
+    {
+        anim.SetBool("isWalking", isWalking);
+        anim.SetFloat("horizontalDirection", lastDirection.x);
+        anim.SetFloat("verticalDirection", lastDirection.y);
+    }
+}
