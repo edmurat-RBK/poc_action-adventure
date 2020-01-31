bfde15703fcb231677d660fa8f20282443065386
Edouard <e.murat@rubika-edu.com>
2020-01-31 14:05:20 +0100

    Commented code

--- Assets/Scripts/Player/PlayerMeleeAttack.cs
+++ Assets/Scripts/Player/PlayerMeleeAttack.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class PlayerMeleeAttack : MonoBehaviour
 {
-    public float baseMeleeAttackDamage = 1f;
-    public float attackModifier = 1f;
+    #region Variables
 
+    //Initial damage of melee attack hit
+    public float baseMeleeAttackDamage = 1f;
+
+    //If true > The player has pressed the melee attack input
     private bool inputAttack;
 
+    //PlayerState component
     private PlayerState playerState;
+    //PlayerModifier component
     private PlayerModifier playerModifier;
 
+    #endregion
+
+    #region Methods
+
+    #region Unity event
+
+    //Unity Start event
     private void Start()
     {
+        //Get PlayerState
         playerState = GetComponent<PlayerState>();
     }
 
+    //Unity Update event
     private void Update()
     {
         if (playerState.CanMeleeAttack)
@@ -29,4 +43,8 @@ public class PlayerMeleeAttack : MonoBehaviour
             }
         }
     }
+
+    #endregion
+
+    #endregion
 }
